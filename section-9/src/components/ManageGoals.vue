093207d852640c99a748085a4f161f09318777f2
John Mello <john.mello@valtech.com>
2023-02-16 14:16:29 -0300

    Teleport
    We use the teleport to say where we want to render a component in the DOM.
    We surround the component with a <teleport> tag and add the to prop and
    pass a valid css selector as a value, it can be an HTML tag, an #id or
    a .class

--- section-9/src/components/ManageGoals.vue
+++ section-9/src/components/ManageGoals.vue
@@ -1,6 +1,39 @@
 <template>
   <div>
     <h2>Manage Goals Component</h2>
-    <input type="text">
+    <input type="text" ref="goal">
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <ErrorModal v-if="inputIsInvalid">
+        <h2>Error</h2>
+        <button @click="closeModal">Gotcha!</button>
+      </ErrorModal>
+    </teleport>
   </div>
 </template>
+
+<script>
+import ErrorModal from './ErrorModal.vue'
+
+export default {
+  components: {
+    ErrorModal
+  },
+  data() {
+    return {
+      inputIsInvalid: false
+    }
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value
+      if (enteredValue === '') {
+        this.inputIsInvalid = true
+      }
+    },
+    closeModal() {
+      this.inputIsInvalid = false
+    }
+  }
+}
+</script>
