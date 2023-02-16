43d0f6bce9e5aa31a02d5c66963d8a7d2ef88add
John Mello <john.mello@valtech.com>
2023-02-16 11:54:38 -0300

    Dynamic components
    Instead of using v-if to check for a value to determine whether we want
    to show one component or another, we can use de <component> tag, and define
    the is attribute.
    The component rendered will be based on the value od the is property.

--- section-9/src/App.vue
+++ section-9/src/App.vue
@@ -2,43 +2,42 @@
   <div>
     <!-- <the-header></the-header> -->
     <TheHeader />
-    <BadgeList />
-    <UserInfo
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></UserInfo>
-    <CourseGoals>
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-      </template>
-    </CourseGoals>
+    <button @click="setSelectedComponent('ActiveGoals')">Active Goals</button>
+    <!-- <button @click="setSelectedComponent('active-goals')">Active Goals</button> -->
+    <!-- <button @click="setSelectedComponent('ManageGoals')">Manage Goals</button> -->
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+
+    <!-- <ActiveGoals v-if="selectedComponent === 'active-goals'" /> -->
+    <!-- <ManageGoals v-if="selectedComponent === 'manage-goals'" /> -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: {
-    // 'the-header': TheHeader,
-    // TheHeader: TheHeader,
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
---
+++ section-9/src/components/ManageGoals.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Manage Goals Component</h2>
+</template>
